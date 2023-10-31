0e1f5378eb37820aaa47e92358018ca133fd4a83
rydnjs98 <98crace@gmail.com>
2023-10-31 22:34:54 +0900

    firebase database 연동

---
+++ app/src/main/java/com/example/recipe_project/Recipe_Post.java
@@ -0,0 +1,40 @@
+package com.example.recipe_project;
+
+import com.google.firebase.database.Exclude;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Recipe_Post {
+    public int ID;
+    public String name;
+    public String link;
+    public String IDs;
+    public String tag;
+    public String info;
+
+    public Recipe_Post() {
+        // Default constructor required for calls to DataSnapshot.getValue(FirebasePost.class)
+    }
+
+    public Recipe_Post(int id, String name, String link, String IDs, String tag, String info) {
+        this.ID = id;
+        this.name = name;
+        this.link = link;
+        this.IDs = IDs;
+        this.tag = tag;
+        this.info = info;
+    }
+
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("recipe_ID", ID);
+        result.put("recipe_name", name);
+        result.put("recipe_link", link);
+        result.put("recipe_ingrediantIDs", IDs);
+        result.put("recipe_tag", tag);
+        result.put("recipe_info", info);
+        return result;
+    }
+}
