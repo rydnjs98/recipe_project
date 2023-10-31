5420b517fa172496b0aa37d83d5da4cb5fbf1ae5
Kimhoky <enwl77@naver.com>
2023-10-31 19:42:48 +0900

    mainfest 수정 및 search 수정

--- app/src/main/java/com/example/recipe_project/MainActivity.java
+++ app/src/main/java/com/example/recipe_project/MainActivity.java
@@ -2,74 +2,44 @@ package com.example.recipe_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button tosearch;
+    Button tofavorite;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       tosearch = findViewById(R.id.main_search_btn);
+       tofavorite = findViewById(R.id.main_favorite_btn);
+
+        tosearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Intent intent = new Intent(MainActivity.this, Search.class);
+               startActivity(intent);
+            }
+        });
+        tofavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Favorite.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
-
-
-
-    /* void getVal(){
-        View root = binding.getRoot();
-
-        //Dbhelper의 읽기모드 객체를 가져와 SQLiteDatabase에 담아 사용준비
-        DataBaseHelper helper = new DataBaseHelper(cscontext.getApplicationContext());
-        SQLiteDatabase database = helper.getReadableDatabase();
-
-
-
-        //Cursor라는 그릇에 목록을 담아주기
-        Cursor cursor = database.rawQuery("SELECT * FROM student" ,null);
-
-        helper.insertData(getTime(),csCheck,SName);
-//        helper.insertTimeData(getTime(),"김경호");
-//        helper.insertCSData(csCheck,"김경호");
-//        if(cursor != null && cursor.moveToFirst())
-        //목록의 개수만큼 순회하여 adapter에 있는 list배열에 add
-        while(cursor.moveToNext()){
-            //num 행은 가장 첫번째에 있으니 0번이 되고, name은 1번
-
-            //  val = cursor.getString(7);
-
-            //val = slideshowViewModel.getprla();
-//            if(isInserted==true)
-//
-
-        }
-
-
-
-        cursor.close();
-        helper.close();
-
-
-
-    } */
-
-   /* public List<User> userList ;
-
-    public void initLoadDB() {
-
-        DataAdapter mDbHelper = new DataAdapter(cscontext.getApplicationContext());
-        mDbHelper.createDatabase();
-        mDbHelper.open();
-
-        // db에 있는 값들을 model을 적용해서 넣는다.
-        userList = mDbHelper.getTableData();
-
-        // db 닫기
-        mDbHelper.close();
-    } */
 
 }
--- app/src/main/java/com/example/recipe_project/Search.java
+++ app/src/main/java/com/example/recipe_project/Search.java
@@ -1,14 +1,20 @@
 package com.example.recipe_project;
 
 import android.os.Bundle;
+import android.widget.Button;
 
-import androidx.annotation.Nullable;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Search extends AppCompatActivity {
+
+    Button tomain;
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+
+        tomain = findViewById(R.id.search_main_btn);
     }
 }
