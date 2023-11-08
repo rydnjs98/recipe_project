2e36041e507669418f793dc2d8607383cb5a7b8b
rydnjs98 <98crace@gmail.com>
2023-11-08 15:52:49 +0900

    레시피 페이지 추가 수정

--- app/src/main/java/com/example/recipe_project/Recipe.java
+++ app/src/main/java/com/example/recipe_project/Recipe.java
@@ -23,7 +23,7 @@ public class Recipe extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.recipes);
+        setContentView(R.layout.activity_recipe);
 
         youTubePlayerView = findViewById(R.id.youtube_player_view);
 
