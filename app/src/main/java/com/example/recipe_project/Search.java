5beffaf3b8d5d6287bbd0acdad107569dfa6b8cc
Kimhoky <enwl77@naver.com>
2023-11-15 15:51:35 +0900

    시간과 날씨 가져오기 성공

--- app/src/main/java/com/example/recipe_project/Search.java
+++ app/src/main/java/com/example/recipe_project/Search.java
@@ -2,7 +2,10 @@ package com.example.recipe_project;
 
 import static com.example.recipe_project.DataAdapter.TAG;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -14,6 +17,7 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import androidx.annotation.NonNull;
@@ -34,9 +38,22 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Search extends AppCompatActivity {
 
@@ -52,11 +69,14 @@ public class Search extends AppCompatActivity {
     List<FindItem> gotlist = new ArrayList<>();
 
 
-    private boolean isCode = false;
-    private boolean isEmail = false;
-    private boolean isUsername = false;
-    private HashMap<String, Object> hashMap = new HashMap<>();
+    long mNow;
+    Date mDate;
+    SimpleDateFormat mFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 
+    Context context;
+    TextView textView1, textView2;
+    String text1, text2, text3;
+    String result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,7 +93,26 @@ public class Search extends AppCompatActivity {
         tag4 = findViewById(R.id.search_textView4);
         dosearch = findViewById(R.id.search_image_btn);
 
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                 result = performWebCrawling();
 
+                // UI 업데이트를 위해 runOnUiThread 사용
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // 결과를 처리하는 부분
+                        if (result != null) {
+                            Log.d("Weather", "Weather: " + result);
+                            // 여기서 가져온 결과를 필요에 맞게 처리하세요.
+                        } else {
+                            Log.e("Weather", "Failed to retrieve weather information");
+                        }
+                    }
+                });
+            }
+        }).start();
 
         RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
 
@@ -92,22 +131,21 @@ public class Search extends AppCompatActivity {
         docref.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if(task.isSuccessful()){
+                if (task.isSuccessful()) {
 
-                    for (QueryDocumentSnapshot doc : task.getResult())
-                    {
+                    for (QueryDocumentSnapshot doc : task.getResult()) {
                         FindItem item = doc.toObject(FindItem.class);
                         searchlist.add(item);
 
                     }
-                    for (FindItem item : searchlist){
+                    for (FindItem item : searchlist) {
                         Log.d(TAG, "recipe name = " + item.getRecipe_Name());
                         Log.d(TAG, "recipe tag = " + item.getRecipe_ID());
                     }
 
                     mRecyclerAdapter.setFindList(searchlist);
 
-                }else{
+                } else {
                     Log.d(TAG, "GET FAILED" + task.getException());
                 }
             }
@@ -130,41 +168,61 @@ public class Search extends AppCompatActivity {
 
 
                 gotlist.clear();
-                if(text.equals("")){
+                if (text.equals("")) {
                     mRecyclerAdapter.setFindList(searchlist);
-                }
-                else{
-                    if(text.equals("일식")){
-                        for(int i=0; i<searchlist.size(); i++){
-                            if(searchlist.get(i).recipe_tag.contains(text)){
+                } else {
+                    if (text.equals("일식")) {
+                        for (int i = 0; i < searchlist.size(); i++) {
+                            if (searchlist.get(i).recipe_tag.contains(text)) {
                                 gotlist.add(searchlist.get(i));
                             }
                             mRecyclerAdapter.setFindList(gotlist);
                         }
-                    }else if(text.equals("양식")){
-                        for(int i=0; i<searchlist.size(); i++){
-                            if(searchlist.get(i).recipe_tag.contains(text)){
+                    } else if (text.equals("양식")) {
+                        for (int i = 0; i < searchlist.size(); i++) {
+                            if (searchlist.get(i).recipe_tag.contains(text)) {
                                 gotlist.add(searchlist.get(i));
                             }
                             mRecyclerAdapter.setFindList(gotlist);
                         }
-                    }else if(text.equals("한식")){
-                        for(int i=0; i<searchlist.size(); i++){
-                            if(searchlist.get(i).recipe_tag.contains(text)){
+                    } else if (text.equals("한식")) {
+                        for (int i = 0; i < searchlist.size(); i++) {
+                            if (searchlist.get(i).recipe_tag.contains(text)) {
                                 gotlist.add(searchlist.get(i));
                             }
                             mRecyclerAdapter.setFindList(gotlist);
                         }
-                    }else if(text.equals("중식")){
-                        for(int i=0; i<searchlist.size(); i++){
-                            if(searchlist.get(i).recipe_tag.contains(text)){
+                    } else if (text.equals("중식")) {
+                        for (int i = 0; i < searchlist.size(); i++) {
+                            if (searchlist.get(i).recipe_tag.contains(text)) {
                                 gotlist.add(searchlist.get(i));
                             }
                             mRecyclerAdapter.setFindList(gotlist);
                         }
-                    }else{
-                        for(int i=0; i<searchlist.size(); i++){
-                            if(searchlist.get(i).recipe_name.contains(text)){
+                    } else if (text.equals("밥")) {
+                        for (int i = 0; i < searchlist.size(); i++) {
+                            if (searchlist.get(i).recipe_tag.contains(text)) {
+                                gotlist.add(searchlist.get(i));
+                            }
+                            mRecyclerAdapter.setFindList(gotlist);
+                        }
+                    } else if (text.equals("면")) {
+                        for (int i = 0; i < searchlist.size(); i++) {
+                            if (searchlist.get(i).recipe_tag.contains(text)) {
+                                gotlist.add(searchlist.get(i));
+                            }
+                            mRecyclerAdapter.setFindList(gotlist);
+                        }
+                    } else if (text.equals("야식")) {
+                        for (int i = 0; i < searchlist.size(); i++) {
+                            if (searchlist.get(i).recipe_tag.contains(text)) {
+                                gotlist.add(searchlist.get(i));
+                            }
+                            mRecyclerAdapter.setFindList(gotlist);
+                        }
+                    } else {
+                        for (int i = 0; i < searchlist.size(); i++) {
+                            if (searchlist.get(i).recipe_name.contains(text)) {
                                 gotlist.add(searchlist.get(i));
                             }
                             mRecyclerAdapter.setFindList(gotlist);
@@ -174,9 +232,6 @@ public class Search extends AppCompatActivity {
                 }
             }
         });
-
-
-
 
 
         tomain.setOnClickListener(new View.OnClickListener() {
@@ -223,16 +278,64 @@ public class Search extends AppCompatActivity {
         });
 
 
+        dosearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getTime();
+                try {
+                    // 예제로 사용할 날짜와 시간을 설정합니다.
 
+
+                    // Calendar 객체를 생성하고 날짜 및 시간을 설정합니다.
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.setTime(mDate);
+
+                    // 시간을 가져옵니다.
+                    int hourOfDay = calendar.get(Calendar.HOUR_OF_DAY);
+
+                    // 아침, 점심, 저녁으로 나누어 처리합니다.
+                    if (hourOfDay >= 6 && hourOfDay < 12) {
+                        System.out.println("아침");
+                    } else if (hourOfDay >= 12 && hourOfDay < 18) {
+                        System.out.println("점심" + result);
+                    } else {
+                        System.out.println("저녁");
+                    }
+
+                } catch (Exception e) {
+
+                }
+
+            }
+        });
 
 
     }
 
+    private String getTime() {
+        mNow = System.currentTimeMillis();
+        mDate = new Date(mNow);
+        return mFormat.format(mDate);
+    }
 
 
 
+    private String performWebCrawling() {
+        try {
+            // Jsoup을 사용하여 웹사이트의 HTML 문서를 가져오기
+            Document doc = Jsoup.connect("https://weather.naver.com/today/13140121?cpName=KMA").get();
 
+            // <span class="weather"> 태그에서 텍스트 가져오기
+            Element weatherElement = doc.select("span.weather").first();
+            if (weatherElement != null) {
+                return weatherElement.text();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-
+        return null;
+    }
 
 }
+
