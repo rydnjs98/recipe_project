27d7077d4091ce790d6d5ad16f0fbfb6fc2dfc06
Kimhoky <enwl77@naver.com>
2023-11-21 20:35:29 +0900

    레시피 이미지 추가 + 서치에서 이미지 적용

--- app/src/main/java/com/example/recipe_project/MyRecyclerAdapter.java
+++ app/src/main/java/com/example/recipe_project/MyRecyclerAdapter.java
@@ -1,6 +1,8 @@
 package com.example.recipe_project;
 
 
+
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +14,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.ViewHolder>{
@@ -76,8 +79,39 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.Vi
         }
 
         void onBind(FindItem item){
-            //profile.setImageResource(item.getRecipe_ID());
+
+            HashMap<Integer, Integer> images = new HashMap<Integer, Integer>();
+
+            images.put( 1, Integer.valueOf( R.drawable.img_1 ) );
+            images.put( 2, Integer.valueOf( R.drawable.img_2 ) );
+            images.put( 3, Integer.valueOf( R.drawable.img_3 ) );
+            images.put( 4, Integer.valueOf( R.drawable.img_4) );
+            images.put( 5, Integer.valueOf( R.drawable.img_5 ) );
+            images.put( 6, Integer.valueOf( R.drawable.img_6 ) );
+            images.put( 7, Integer.valueOf( R.drawable.img_7 ) );
+            images.put( 8, Integer.valueOf( R.drawable.img_8 ) );
+            images.put( 9, Integer.valueOf( R.drawable.img_9) );
+            images.put( 10, Integer.valueOf( R.drawable.img_10 ) );
+            images.put( 11, Integer.valueOf( R.drawable.img_11 ) );
+            images.put( 12, Integer.valueOf( R.drawable.img_12 ) );
+            images.put( 13, Integer.valueOf( R.drawable.img_13 ) );
+            images.put( 14, Integer.valueOf( R.drawable.img_14 ) );
+            images.put( 15, Integer.valueOf( R.drawable.img_15 ) );
+            images.put( 16, Integer.valueOf( R.drawable.img_16 ) );
+            images.put( 17, Integer.valueOf( R.drawable.img_17 ) );
+            images.put( 18, Integer.valueOf( R.drawable.img_18 ) );
+            images.put( 19, Integer.valueOf( R.drawable.img_19 ) );
+            images.put( 20, Integer.valueOf( R.drawable.img_20 ) );
+
+
+
+
+
+
+
+            profile.setImageResource(images.get(item.getRecipe_ID()).intValue() );
             name.setText(item.getRecipe_Name());
+
 
         }
 
