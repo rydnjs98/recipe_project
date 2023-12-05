fde532e8dca817595d58ee5e33ba7ab1d87bfd63
Kimhoky <enwl77@naver.com>
2023-12-05 20:41:34 +0900

    어플 아이콘 수정 및 splash 이미지 추가

---
+++ app/src/main/java/com/example/recipe_project/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.recipe_project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Optional: Add any initialization code here
+
+        // Delay for a few seconds and then start the main activity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, 2000); // 2000 milliseconds = 2 seconds
+    }
+}
